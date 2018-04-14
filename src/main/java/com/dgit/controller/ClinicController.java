4d731e0cc1c9d0de2dc4d5737fbf003ee1459a28
HongBum <HongBum@LAPTOP-M2S4SSKH>
2018-04-14 19:44:46 +0900

    클리닉 완료

--- src/main/java/com/dgit/controller/ClinicController.java
+++ src/main/java/com/dgit/controller/ClinicController.java
@@ -1,19 +1,45 @@
 package com.dgit.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.dgit.domain.ClinicVO;
+import com.dgit.domain.SearchCriteria;
+import com.dgit.service.ClinicService;
+
 @Controller
 @RequestMapping("/clinic")
 public class ClinicController {
-private static final Logger logger = LoggerFactory.getLogger(ClinicController.class);
+	private static final Logger logger = LoggerFactory.getLogger(ClinicController.class);
 	
-	@RequestMapping(value="clinic",method=RequestMethod.GET)
-	public void getClassboard(){
+	@Autowired
+	public ClinicService service;
+	@RequestMapping(value = "clinic", method = RequestMethod.GET)
+	public void getClassboard() {
 		logger.info("=================clinic Get====================");
+
+	}
+
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	public ResponseEntity<List<ClinicVO>> list(SearchCriteria cri, String type) {
+		ResponseEntity<List<ClinicVO>> entity = null;
 		
+		try{
+			cri.setSearchType(type);
+			List<ClinicVO> lists = service.list(cri);
+			entity = new ResponseEntity<>(lists,HttpStatus.OK);
+		}catch(Exception e){
+			e.printStackTrace();
+			entity = new ResponseEntity<>(HttpStatus.OK);
+		}
+		return entity;
 	}
 }

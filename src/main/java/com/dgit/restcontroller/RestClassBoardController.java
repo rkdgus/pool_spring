2eda1bac72afeb546107a4b09994da26ed86a0f7
HongBumHa <goldstate@naver.com>
2018-04-17 15:43:16 +0900

    레스트 컨트롤러 생성

---
+++ src/main/java/com/dgit/restcontroller/RestClassBoardController.java
@@ -0,0 +1,39 @@
+package com.dgit.restcontroller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dgit.domain.ClassVO;
+import com.dgit.service.ClassBoardService;
+
+@RestController
+@RequestMapping("/restclassboard/*")
+public class RestClassBoardController {
+	private static final Logger logger = LoggerFactory.getLogger(RestClassBoardController.class);
+	@Autowired
+	private ClassBoardService service;
+	
+	@RequestMapping(value="/classlist",method=RequestMethod.GET)
+	public ResponseEntity<List<ClassVO>> getClasslist(){
+		logger.info("GET classList");
+		ResponseEntity<List<ClassVO>> entity = null;
+		
+		try{
+			List<ClassVO> lists = service.selectByClass();
+			entity = new ResponseEntity<List<ClassVO>>(lists,HttpStatus.OK);
+		}catch(Exception e){
+			e.printStackTrace();
+			entity = new ResponseEntity<List<ClassVO>>(HttpStatus.OK);
+		}
+		
+		return entity;
+	}
+}

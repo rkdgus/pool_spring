767bfeb9fc4ac75e47aeb4f22a74294ceaa99555
HongBum Ha <goldstate@naver.com>
2018-04-29 15:18:38 +0900

    클리닉 시작

---
+++ src/main/java/com/dgit/restcontroller/RestClinicController.java
@@ -0,0 +1,47 @@
+package com.dgit.restcontroller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dgit.domain.ClinicVO;
+import com.dgit.domain.SearchCriteria;
+import com.dgit.service.ClinicService;
+
+@RestController
+@RequestMapping("/clinic/*")
+public class RestClinicController {
+	private static final Logger logger = LoggerFactory.getLogger(RestBusController.class);
+	
+	@Autowired
+	private ClinicService service;
+	
+	@RequestMapping(value = "/clinic", method = RequestMethod.GET)
+	public ResponseEntity<HashMap<String,Object>> bus(SearchCriteria cri) {
+		ResponseEntity<HashMap<String,Object>> entity = null;
+		HashMap<String,Object> map = new HashMap<>();
+		String[] typeName = {"freestyle","backstroke","breaststroke","butterfly","startAndTrun"};
+		
+		try{
+			for(int i=0; i< 5; i++){
+				String name = "list_"+i;
+				cri.setSearchType(typeName[i]);
+				List<ClinicVO> lists = service.list(cri);
+				map.put(name,lists);
+			}
+			entity = new ResponseEntity<HashMap<String,Object>>(map,HttpStatus.OK);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return entity;
+		
+	}
+}
